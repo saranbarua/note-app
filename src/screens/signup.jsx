3a2354a3e68179257270696cabaeb9c059f1b9d6
Saran barua <Saranbarua2357@gmail.com>
2022-09-05 15:07:03 +0600

    updated

--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import {  Text, View, TextInput, StyleSheet, Pressable ,TouchableOpacity} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Button from '../components/button'
 
-export default function Signup() {
+const gender= ['Male','Female']
+
+export default function Signup({navigation}) {
+ 
+const [Gender,SetGender]= useState(null);
   return (
+  
     <SafeAreaView>
       <View style= {{ paddingHorizontal:16,  paddingVertical:25 }}>
         <TextInput placeholder="Email Address" style= {styles.input} />
         <TextInput placeholder="Password" style= {styles.input} secureTextEntry />
-        <Button  title="Signup" customStyles={{ marginTop: 25, alignSelf: 'center' }}   />
-      
+        <TextInput placeholder="Full name" style= {styles.input} />
+        <TextInput placeholder="Age" style= {styles.input} />
+        <Text style={{ marginBottom: 15, fontWeight:"bold" }}>Select your gender</Text>
 
-      <Pressable 
+       {gender.map((Option)=>{
+        const selected= Option === Gender
+        return(
+          <Pressable onPress={()=> SetGender(Option)} key={Option} style={styles.radioContainer}>
+          <View style={[styles.outer, 
+            selected && styles.selectedouter]}>
+        <View style={[styles.inner, 
+          selected && styles.selectedinner]}/>
+          </View>
+         <Text style={styles.radioText}>{Option}</Text>
+         </Pressable>
+        )
+       })} 
+
+
+       
+        
+        <Button  title="Signup" customStyles={{ marginTop: 25, alignSelf: 'center' }}   />
+      <Pressable onPress={()=>{navigation.navigate('Signin')}}
         style={{ marginTop: 25 }}>
                 <Text style={{ textAlign: 'center' }}>
                     Already have an account? {""}<Text style={{ color: '#18B18D', fontWeight: 'bold' }}>Sign In</Text>
@@ -31,5 +55,37 @@ input:{
     marginBottom:25,
     padding: 10,
 
+},
+radioContainer:{
+  flexDirection:"row",
+  alignItems:"center",
+  marginBottom:10
+},
+outer:{
+  height:30,
+  width:30,
+  borderRadius:15,
+  borderWidth:1,
+  borderColor: "#cfcfcf",
+  
+  justifyContent: "center",
+  alignItems:"center"
+},
+inner:{
+  height:15,
+  width:15,
+  borderRadius:7.5,
+  borderWidth:1,
+  borderColor: "#cfcfcf",
+},
+radioText:{
+  marginLeft:10
+},
+selectedouter:{
+  borderColor:"orange"
+},
+selectedinner:{
+  backgroundColor:"orange",
+  borderColor: "orange"
 }
 })
