711355348b846063860898b0848a2f53f8fa74e2
Saran barua <Saranbarua2357@gmail.com>
2022-09-08 16:46:39 +0600

    new

--- src/screens/create.jsx
+++ src/screens/create.jsx
@@ -6,9 +6,9 @@ import Input from "../components/input";
 
 const OPTIONS = ['red', "green", 'blue']
 export default function Create({navigation,route,user}) {
-  const [Title, SetTitle] = React.useState('')
+  const [Title, SetTitle] = React.useState('') 
   const [Description, SetDescription] = React.useState('')
-  
+
   const [noteColor, setNoteColor] = React.useState('white'); 
   return (
     <SafeAreaView style={{marginHorizontal:20, flex:1}}>
