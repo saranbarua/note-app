3b0662a1bc897b177d0fc01e2fe227524a4e6c48
Saran barua <Saranbarua2357@gmail.com>
2022-09-09 19:50:06 +0600

    updated

--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -57,7 +57,6 @@ export default function Signup() {
       <View style={{ paddingHorizontal: 16, paddingVertical: 25 }}>
         <Input
           placeholder="Email Address"
-    
       
           onChangeText={(text) => SetEmail(text)}
           style={styles.input}
