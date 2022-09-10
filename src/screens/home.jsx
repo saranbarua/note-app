dad90bf8a347813008ea1c194e3d8b16b117ca19
Saran barua <Saranbarua2357@gmail.com>
2022-09-10 12:17:18 +0600

    updae

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useState,useEffect } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {AntDesign} from "@expo/vector-icons";
-
+import { query,where,collection, onSnapshot } from "firebase/firestore";
+import { db } from '../../App';
 
 
 export default  function Home({navigation,route,user}) {
   // console.log(user)
+const [Notes,SetNote]= useState([]);
+useEffect(()=>{
+  const que= query(collection(db,'notes'), where("uid", "==", user.uid))
+ const Notelister= onSnapshot(que,(querySnapshot)=>{
+ const list=[];
+  querySnapshot.forEach((doc)=>{
+    list.push(doc.data())
+  })
+  SetNote(list);
+ })
+ return Notelister;
+},[]);
+
+console.log(Notes)
   const onpressToCreate=() => {
     navigation.navigate("Create")
   }
@@ -22,6 +37,11 @@ export default  function Home({navigation,route,user}) {
 <Pressable onPress={onpressToCreate} >
   <AntDesign name='pluscircle' size={24} ></AntDesign>
   </Pressable>
+    </View>
+    <View>
+      <FlatList>
+        
+      </FlatList>
     </View>
     
    </SafeAreaView>
